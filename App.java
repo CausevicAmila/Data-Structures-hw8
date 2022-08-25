9a827f3da25c70a309c6556c386527adba712e78
Amila Čaušević <112026447+CausevicAmila@users.noreply.github.com>
2022-08-26 00:27:18 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,51 @@
+package ibu.edu.homework8BinarySearchTrees;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+
+public class App {
+
+	public static void main(String[] args) throws IOException {
+		Student[] students = new Student[1000000];
+		int i = 0;
+		
+		//to read csv data and put into an array
+		BufferedReader csvReader = new BufferedReader(new FileReader("/Users/acaus/Downloads/Global_University_Students.csv/"));
+		while (i < students.length) {
+
+			String row = csvReader.readLine();
+
+		    String[] rowSplit = row.split(";");
+
+		    students[i]= new Student(Integer.parseInt(rowSplit[0]), rowSplit[1], rowSplit[2], rowSplit[3], 
+		    		rowSplit[4], rowSplit[5], Integer.parseInt(rowSplit[6]));
+		    i++;
+		}
+
+		csvReader.close();
+		
+		binarySearchTree<Integer, Student> bst = new binarySearchTree<Integer, Student>();
+		
+		//Putting the students from the array into the tree
+		for (int j = 0; j < students.length; j++) {
+			bst.put(students[j].getStudentID(), students[j]);
+		}
+		
+		//Console prompts
+		System.out.println("Enter the ID of the student you want to retrieve: ");
+		Scanner scan = new Scanner(System.in);
+		int id = scan.nextInt();
+		scan.close();
+		Student student = bst.get(id);
+		if (student != null) {
+			System.out.println(student);
+			System.out.println("The student was retrieved from BST in " + bst.getSteps() + " steps");
+		} else {
+			System.out.println("The student with the given ID does not exist");
+			System.out.println("The search was completed in " + bst.getSteps() + " steps");
+		}
+	}
+}
